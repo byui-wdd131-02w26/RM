d82ea6935c76cd047b78cfeecbdcb5695b4ee23e
rugenmyrto <myrtorugen1@gmail.com>
2026-01-16 18:43:36 -0700

    hyr

---
+++ images/script.js
@@ -0,0 +1,17 @@
+let selectElem = document.querySelector('#theme-select');
+let logo = document.querySelector('#byui-logo');
+
+selectElem.addEventListener('change', changeTheme);
+
+function changeTheme() {
+  let current = selectElem.value;
+
+  if (current === 'dark') {
+    document.body.classList.add('dark');
+    logo.src = 'images/byui-logo-white.png';
+  } else {
+    document.body.classList.remove('dark');
+    logo.src = 'images/byulogo.webp';
+  }
+}
+
